90085223a708fde1975ad6413119a0681b1a5437
Robin Thellend <rthellend@google.com>
2014-05-12 12:01:34 -0700

    veyron/runtimes/google/daemon/proximityd: Add authorizer.
    Add a FileACLAuthorizer to proximityd to control access to the service's
    method. The RegisterName and Unregister methods require Write access. The
    NearbyDevices method requires Read access.
    
    Change-Id: I3b0a47c3879e5d8f46b9e8e87854b2f07d1779c7

--- services/proximity/proximity.idl.go
+++ services/proximity/proximity.idl.go
@@ -4,6 +4,8 @@
 package proximity
 
 import (
+	"veyron2/security"
+
 	// The non-user imports are prefixed with "_gen_" to prevent collisions.
 	_gen_idl "veyron2/idl"
 	_gen_ipc "veyron2/ipc"
@@ -261,11 +263,11 @@ func (__gen_s *ServerStubProximity) NearbyDevices(call _gen_ipc.ServerCall) (rep
 func GetProximityMethodTags(method string) []interface{} {
 	switch method {
 	case "RegisterName":
-		return []interface{}{}
+		return []interface{}{security.Label(2)}
 	case "UnregisterName":
-		return []interface{}{}
+		return []interface{}{security.Label(2)}
 	case "NearbyDevices":
-		return []interface{}{}
+		return []interface{}{security.Label(1)}
 	default:
 		return nil
 	}
