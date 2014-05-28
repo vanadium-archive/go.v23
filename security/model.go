e20e8faf2c9f3bae3e24a74c78bb54b9b68630ad
Ankur <ataly@google.com>
2014-05-28 13:35:55 -0700

    "veyron/runtimes/google/security": Only return trusted Names.
    This CL makes the following changes:
    * changes PublicID.Names() such that it only returns those names that
    are rooted at Trusted identity providers.
    * changes PublicID.Match(...) such that if the PublicID has no trusted names
    then the PublicID only matches the pattern "*".
    
    With these changes we no longer rely on developers not using untrusted
    patterns (i.e., patterns beginning with "untrusted") in their ACLs.
    
    Change-Id: Iae9036cf0bd12dc947f09a439f8942b70cec2d25

--- security/model.go
+++ security/model.go
@@ -19,9 +19,9 @@ type PublicID interface {
 	// or can obtain a name matching the PrincipalPattern by manipulating its PublicID
 	// using PrivateID operations (e.g., <PrivateID>.Bless(<PublicID>, ..., ...)). The
 	// provided pattern may be of one of the following forms:
-	// - pattern "*" identifying all names.
-	// - a specific name <name> identifying all names that can be extended to <name>.
-	// - pattern <name>/* identifying all extensions of the name <name>.
+	// - pattern "*" matching all principals regardless of the names they have.
+	// - a specific name <name> matching all principals who have a name that can be extended to <name>.
+	// - pattern <name>/* matching all principals who have a name that is an extension of the name <name>.
 	Match(pattern PrincipalPattern) bool
 
 	// PublicKey returns the public key corresponding to the private key
