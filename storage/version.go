00dc5b6e3b800d2d3d36ce75cca0d98407eeef8f
Himabindu Pucha <hpucha@google.com>
2014-06-10 11:50:03 -0700

    veyron2/storage: Seeds the store version number generator to produce random version numbers across different instances of store.
    This is a bug fix: When two stores were generating updates, they were
    generating the same version numbers, causing collisions.
    
    Change-Id: I63fe68fe8d082f699afed6b5e0a9ca0eb6e917b3

--- storage/version.go
+++ storage/version.go
@@ -2,14 +2,21 @@ package storage
 
 import (
 	"math/rand"
+	"time"
 )
+
+var rng *rand.Rand
+
+func init() {
+	rng = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+}
 
 // NewVersion returns a new version number.
 //
 // TODO(jyh): Choose a better version generator.
 func NewVersion() Version {
 	for {
-		if v := Version(rand.Int63()); v != 0 {
+		if v := Version(rng.Int63()); v != 0 {
 			return v
 		}
 	}
