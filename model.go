aee20b931956ce77273abb40aaa9879e0087f6a5
Matt Rosencrantz <mattr@google.com>
2014-08-25 10:54:11 -0700

    veyron2: Remove TODOContext.
    I've wired through real contexts through most remaining places.  The
    only thing left is in the Java and Javascript bridges.  There I've
    left TODO comments instead of using TODOContext.
    
    Change-Id: I5d1de417d0022a531a5d046e6653f3514c38973f

--- model.go
+++ model.go
@@ -202,14 +202,6 @@ type Runtime interface {
 	// to ongoing RPCs.
 	NewContext() context.T
 
-	// TODOContext is a factory function to generate a new client context.
-	// This should be used when some context should be supplied but you aren't yet
-	// ready to fill in the correct value.  The idea is that no TODO context
-	// should remain in the codebase long-term.
-	// TODO(mattr): Remove this method entirely once the whole tree has Context
-	// piped through it.
-	TODOContext() context.T
-
 	// NewStreamManager creates a new stream manager.
 	NewStreamManager(opts ...stream.ManagerOpt) (stream.Manager, error)
 
