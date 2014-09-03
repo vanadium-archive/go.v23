7b2983f39aa244f642ff136bfd5210acda43e423
Srdjan Petrovic <spetrovic@google.com>
2014-09-03 09:49:57 -0700

    veyron2/vdl/codegen/java: Fix short constants handling.
    Change-Id: Iaed2341589413ebb27e28b9f14131365f9d6db14

--- vdl/codegen/java/util_val.go
+++ vdl/codegen/java/util_val.go
@@ -32,18 +32,18 @@ func javaVal(v *vdl.Value, env *compile.Env) string {
 		}
 	case vdl.Byte:
 		return "(byte)" + strconv.FormatUint(uint64(v.Byte()), 10)
-	case vdl.Uint16, vdl.Uint32, vdl.Uint64:
-		c := strconv.FormatUint(v.Uint(), 10)
-		if v.Kind() == vdl.Uint64 {
-			return c + longSuffix
-		}
-		return c
-	case vdl.Int16, vdl.Int32, vdl.Int64:
-		c := strconv.FormatInt(v.Int(), 10)
-		if v.Kind() == vdl.Int64 {
-			return c + longSuffix
-		}
-		return c
+	case vdl.Uint16:
+		return "(short)" + strconv.FormatUint(v.Uint(), 10)
+	case vdl.Int16:
+		return "(short)" + strconv.FormatInt(v.Int(), 10)
+	case vdl.Uint32:
+		return strconv.FormatUint(v.Uint(), 10)
+	case vdl.Int32:
+		return strconv.FormatInt(v.Int(), 10)
+	case vdl.Uint64:
+		return strconv.FormatUint(v.Uint(), 10) + longSuffix
+	case vdl.Int64:
+		return strconv.FormatInt(v.Int(), 10) + longSuffix
 	case vdl.Float32, vdl.Float64:
 		c := strconv.FormatFloat(v.Float(), 'g', -1, bitlen(v.Kind()))
 		if strings.Index(c, ".") == -1 {
