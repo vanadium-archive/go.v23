d18de92f264501ceeb56b6c217d3afd359a18a96
Asim Shankar <ashankar@google.com>
2014-09-10 13:33:23 -0700

    veyron2/security,veyron/security: Fix bug in signature generation.
    The ECDSA signature algorithm signs only the leftmost N bits of a message
    where N is the size of the underlying field of the curve. For example,
    a P256 curve signs the leftmost 32 bits.
    
    Prior to this commit, NewClearSigner.Sign would effectively only
    sign the leftmost bits of the message, thereby allowing signatures
    of one message >N bits to be used for distinct messages that share
    the first N bits.
    
    This commit fixes this problem by using a cryptographic hash function
    on the message (if necessary) to "reduce" the size of the data
    being signed to N bits.
    
    The test added to signature_test.go is more comprehensive than
    the tests it is replacing in this commit and would fail without
    the changes to apply the hash function.
    
    This commit also moves NewClearSigner from the veyron/security/signing
    package to veyron2/security.
    
    Change-Id: I4b9adcd467165e3eddb18a116fe0165377e7b198

--- security/signature.go
+++ security/signature.go
@@ -2,6 +2,7 @@ package security
 
 import (
 	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
@@ -20,6 +21,42 @@ func (sig Signature) Verify(key PublicKey, message []byte) bool {
 	default:
 		return false
 	}
+}
+
+// NewInMemoryECDSASigner creates a Signer that uses the provided ECDSA private
+// key to sign messages.  This private key is kept in the clear in the memory
+// of the running process.
+func NewInMemoryECDSASigner(key *ecdsa.PrivateKey) Signer {
+	var hash Hash
+	if nbits := key.Curve.Params().BitSize; nbits <= 160 {
+		hash = SHA1Hash
+	} else if nbits <= 256 {
+		hash = SHA256Hash
+	} else if nbits <= 384 {
+		hash = SHA384Hash
+	} else {
+		hash = SHA512Hash
+	}
+	return &clearSigner{key, NewECDSAPublicKey(&key.PublicKey), hash}
+}
+
+type clearSigner struct {
+	key    *ecdsa.PrivateKey
+	pubkey PublicKey
+	hash   Hash
+}
+
+func (c *clearSigner) Sign(message []byte) (sig Signature, err error) {
+	r, s, err := ecdsa.Sign(rand.Reader, c.key, cryptoHash(c.hash, message))
+	if err != nil {
+		return
+	}
+	sig.R, sig.S, sig.Hash = r.Bytes(), s.Bytes(), c.hash
+	return
+}
+
+func (c *clearSigner) PublicKey() PublicKey {
+	return c.pubkey
 }
 
 // cryptoHash hashes the provided data using the given cryptographic hash
--- security/signature_test.go
+++ security/signature_test.go
@@ -4,66 +4,71 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"fmt"
 	"testing"
 )
 
-func newSigner(hash Hash) *signer {
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		panic("couldn't generate private ECDSA key")
+func TestSignature(t *testing.T) {
+	curves := []elliptic.Curve{
+		elliptic.P224(),
+		elliptic.P256(),
+		elliptic.P384(),
+		elliptic.P521(),
 	}
-	return &signer{
-		hash: hash,
-		key:  key,
-	}
-}
-
-type signer struct {
-	hash Hash
-	key  *ecdsa.PrivateKey
-}
-
-func (sn *signer) Sign(message []byte) (Signature, error) {
-	if message = cryptoHash(sn.hash, message); message == nil {
-		return Signature{}, fmt.Errorf("couldn't sign message")
-	}
-	r, s, err := ecdsa.Sign(rand.Reader, sn.key, message)
-	if err != nil {
-		return Signature{}, err
-	}
-	return Signature{
-		Hash: sn.hash,
-		R:    r.Bytes(),
-		S:    s.Bytes(),
-	}, nil
-}
-
-func (sn *signer) PublicKey() PublicKey {
-	return &ecdsaPublicKey{&sn.key.PublicKey}
-}
-
-func TestVerify(t *testing.T) {
-	testcases := [][]byte{
-		[]byte{},
-		[]byte("iwannaruniwannhide"),
-	}
-	signers := []*signer{
-		newSigner(NoHash),
-		newSigner(SHA1Hash),
-		newSigner(SHA256Hash),
-		newSigner(SHA384Hash),
-		newSigner(SHA512Hash),
-	}
-	for _, testcase := range testcases {
-		for _, s := range signers {
-			sig, err := s.Sign(testcase)
-			if err != nil {
-				t.Errorf("couldn't sign message %q using signer %q: %v", testcase, s.hash, err)
-				continue
-			}
-			if !sig.Verify(s.PublicKey(), testcase) {
-				t.Errorf("couldn't verify signature for testcase %s", testcase)
+	for _, curve := range curves {
+		nbits := curve.Params().BitSize
+		key, err := ecdsa.GenerateKey(curve, rand.Reader)
+		if err != nil {
+			t.Errorf("Failed to generate key for curve of %d bits: %v", nbits, err)
+			continue
+		}
+		// Sign a message nbits long and then ensure that a larger message does not have the same signature.
+		message := make([]byte, nbits>>3)
+		signer := NewInMemoryECDSASigner(key)
+		sig, err := signer.Sign(message)
+		if err != nil {
+			t.Errorf("Failed to generate signature with curve of %d bits: %v", nbits, err)
+			continue
+		}
+		if !sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("Signature verification failed with curve of %d bits", nbits)
+			continue
+		}
+		message = append(message, 1)
+		if sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("Signature of message of %d bytes incorrectly verified with curve of %d bits", len(message), nbits)
+			continue
+		}
+		// Sign a message larger than nbits and verify that switching the last byte does not end up with the same signature.
+		if sig, err = signer.Sign(message); err != nil {
+			t.Errorf("Failed to generate signature with curve of %d bits: %v", nbits, err)
+			continue
+		}
+		if !sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("Signature verification failed with curve of %d bits", nbits)
+			continue
+		}
+		message[len(message)-1] = message[len(message)-1] + 1
+		if sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("Signature of modified message incorrectly verified with curve of %d bits", len(message), nbits)
+			continue
+		}
+		// Signing small messages and then extending them.
+		nbytes := (nbits + 7) / 3
+		message = make([]byte, 1, nbytes)
+		if sig, err = signer.Sign(message); err != nil {
+			t.Errorf("Failed to generate signature with curve of %d bits: %v", nbits, err)
+			continue
+		}
+		if !sig.Verify(signer.PublicKey(), message) {
+			t.Errorf("Failed to verify signature of small message with curve of %d bits", nbits)
+			continue
+		}
+		for len(message) < nbytes {
+			// Extended message should fail verification
+			message = append(message, message...)
+			if sig.Verify(signer.PublicKey(), message) {
+				t.Errorf("Signature of extended message (%d bytes) incorrectly verified with curve of %d bits", len(message), nbits)
+				break
 			}
 		}
 	}
