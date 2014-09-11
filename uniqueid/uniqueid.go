77dd07e5c22b7a63d502ec02005a402d396d0f43
Matt Rosencrantz <mattr@google.com>
2014-09-11 13:41:11 -0700

    veyron2/uniqueid: move uniqueid to veyron2 so I can directly depend on it in vtrace.
    Change-Id: I6ddf79c809c6b6b83e1593cd3f111aa3a25b1986

---
+++ uniqueid/uniqueid.go
@@ -0,0 +1,53 @@
+// Package uniqueid helps generate identifiers that are likely to be
+// globally unique.  We want to be able to generate many IDs quickly,
+// so we make a time/space tradeoff.  We reuse the same random data
+// many times with a counter appended.  Note: these IDs are NOT useful
+// as a security mechanism as they will be predictable.
+package uniqueid
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"sync"
+)
+
+var random = RandomGenerator{}
+
+// Valid returns true if the given ID is valid.
+func Valid(id ID) bool {
+	return id != ID{}
+}
+
+// A RandomGenerator can generate random IDs.
+// The zero value of RandomGenerator is ready to use.
+type RandomGenerator struct {
+	mu     sync.Mutex
+	id     ID
+	count  uint16
+	resets int
+}
+
+// NewID produces a new probably unique identifier.
+func (g *RandomGenerator) NewID() (ID, error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	if g.count == 0 {
+		// Either the generator is uninitialized or the counter
+		// has wrapped.  We need a new random prefix.
+		if _, err := rand.Read(g.id[:14]); err != nil {
+			return ID{}, err
+		}
+		// We skip the 0 to ensure that the zero value for ID is
+		// never generated.
+		g.count = 1
+		g.resets++
+	}
+	binary.BigEndian.PutUint16(g.id[14:], g.count)
+	g.count++
+	return g.id, nil
+}
+
+// Random produces a new probably unique identifier using the RandomGenerator.
+func Random() (ID, error) {
+	return random.NewID()
+}
