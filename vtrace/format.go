041802f8717790106c05c1ceb3814bd16997427d
Matt Rosencrantz <mattr@google.com>
2014-11-10 09:38:57 -0800

    veyron/runtimes/google/vtrace: Add a store for collecting vtrace traces.
    Also optionally allow users to dump all stored traces when the runtime is shut down.
    
    Change-Id: I16e1a19d3bbc21653718445ae00c8a3496189a53

---
+++ vtrace/format.go
@@ -0,0 +1,142 @@
+package vtrace
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"veyron.io/veyron/veyron2/uniqueid"
+)
+
+const indentStep = "    "
+
+type node struct {
+	span     *SpanRecord
+	children []*node
+}
+
+// TODO(mattr): It is useful in general to make a tree of spans
+// for analysis as well as formatting.  This interface should
+// be cleaned up and exported.
+func buildTree(trace *TraceRecord) *node {
+	var root *node
+	var earliestTime int64
+	nodes := make(map[uniqueid.ID]*node, len(trace.Spans))
+
+	for i := range trace.Spans {
+		span := &trace.Spans[i]
+		if earliestTime == 0 || span.Start < earliestTime {
+			earliestTime = span.Start
+		}
+
+		n := nodes[span.ID]
+		if n == nil {
+			n = &node{}
+			nodes[span.ID] = n
+		}
+
+		n.span = span
+
+		if span.Parent == trace.ID {
+			root = n
+		} else {
+			p := nodes[span.Parent]
+			if p == nil {
+				p = &node{}
+				nodes[span.Parent] = p
+			}
+			p.children = append(p.children, n)
+		}
+	}
+
+	// If we didn't find the root span of the trace
+	// create a stand-in.
+	if root == nil {
+		root = &node{
+			span: &SpanRecord{
+				Name:  "Missing Root Span",
+				Start: earliestTime,
+			},
+		}
+	}
+
+	// Find all nodes that have no span.  These represent missing data
+	// in the tree.  We invent fake "missing" spans to represent
+	// (perhaps several) layers of missing spans.  Then we add these as
+	// children of the root.
+	var missing []*node
+	for _, n := range nodes {
+		if n.span == nil {
+			n.span = &SpanRecord{
+				Name: "Missing Data",
+			}
+			missing = append(missing, n)
+		}
+	}
+
+	if len(missing) > 0 {
+		root.children = append(root.children, missing...)
+	}
+
+	return root
+}
+
+func formatDelta(when, start int64) string {
+	if when == 0 {
+		return "??"
+	}
+	return time.Duration(when - start).String()
+}
+
+func formatNode(w io.Writer, n *node, traceStart int64, indent string) {
+	fmt.Fprintf(w, "%sSpan - %s [id: %x parent %x] (%s, %s)\n",
+		indent,
+		n.span.Name,
+		n.span.ID[12:],
+		n.span.Parent[12:],
+		formatDelta(n.span.Start, traceStart),
+		formatDelta(n.span.End, traceStart))
+	indent += indentStep
+	for _, a := range n.span.Annotations {
+		fmt.Fprintf(w, "%s@%s %s\n", indent, formatDelta(a.When, traceStart), a.Message)
+	}
+	for _, c := range n.children {
+		formatNode(w, c, traceStart, indent)
+	}
+}
+
+func formatTime(when int64, loc *time.Location) string {
+	if when == 0 {
+		return "??"
+	}
+	t := time.Unix(0, when)
+	if loc != nil {
+		t = t.In(loc)
+	}
+	return t.Format("2006-01-02 15:04:05.000000 MST")
+}
+
+// FormatTrace writes a text description of the given trace to the
+// given writer.  Times will be formatted according to the given
+// location, if loc is nil local times will be used.
+func FormatTrace(w io.Writer, record *TraceRecord, loc *time.Location) {
+	if root := buildTree(record); root != nil {
+		fmt.Fprintf(w, "Trace - %x (%s, %s)\n",
+			record.ID[12:],
+			formatTime(root.span.Start, loc),
+			formatTime(root.span.End, loc))
+		for _, c := range root.children {
+			formatNode(w, c, root.span.Start, indentStep)
+		}
+	}
+}
+
+// FormatTraces writes a text description of all the given traces to
+// the given writer.  Times will be formatted according to the given
+// location, if loc is nil local times will be used.
+func FormatTraces(w io.Writer, records []*TraceRecord, loc *time.Location) {
+	fmt.Fprintf(w, "Vtrace traces:\n")
+	for _, record := range records {
+		FormatTrace(w, record, loc)
+	}
+}
---
+++ vtrace/format_test.go
@@ -0,0 +1,158 @@
+package vtrace_test
+
+import (
+	"bytes"
+	"encoding/binary"
+	"testing"
+	"time"
+
+	"veyron.io/veyron/veyron2/uniqueid"
+	"veyron.io/veyron/veyron2/vtrace"
+)
+
+var nextid = uint64(1)
+
+func id() uniqueid.ID {
+	var out uniqueid.ID
+	binary.BigEndian.PutUint64(out[8:], nextid)
+	nextid++
+	return out
+}
+
+func TestFormat(t *testing.T) {
+	trid := id()
+	trstart := time.Date(2014, 11, 6, 13, 1, 22, 400000000, time.UTC)
+	spanIDs := make([]uniqueid.ID, 4)
+	for i := range spanIDs {
+		spanIDs[i] = id()
+	}
+	tr := vtrace.TraceRecord{
+		ID: trid,
+		Spans: []vtrace.SpanRecord{
+			{
+				ID:     spanIDs[0],
+				Parent: trid,
+				Name:   "",
+				Start:  trstart.UnixNano(),
+			},
+			{
+				ID:     spanIDs[1],
+				Parent: spanIDs[0],
+				Name:   "Child1",
+				Start:  trstart.Add(time.Second).UnixNano(),
+				End:    trstart.Add(10 * time.Second).UnixNano(),
+			},
+			{
+				ID:     spanIDs[2],
+				Parent: spanIDs[0],
+				Name:   "Child2",
+				Start:  trstart.Add(20 * time.Second).UnixNano(),
+				End:    trstart.Add(30 * time.Second).UnixNano(),
+			},
+			{
+				ID:     spanIDs[3],
+				Parent: spanIDs[1],
+				Name:   "GrandChild1",
+				Start:  trstart.Add(3 * time.Second).UnixNano(),
+				End:    trstart.Add(8 * time.Second).UnixNano(),
+				Annotations: []vtrace.Annotation{
+					{
+						Message: "First Annotation",
+						When:    trstart.Add(4 * time.Second).UnixNano(),
+					},
+					{
+						Message: "Second Annotation",
+						When:    trstart.Add(6 * time.Second).UnixNano(),
+					},
+				},
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	vtrace.FormatTrace(&buf, &tr, time.UTC)
+	want := `Trace - 00000001 (2014-11-06 13:01:22.400000 UTC, ??)
+    Span - Child1 [id: 00000003 parent 00000002] (1s, 10s)
+        Span - GrandChild1 [id: 00000005 parent 00000003] (3s, 8s)
+            @4s First Annotation
+            @6s Second Annotation
+    Span - Child2 [id: 00000004 parent 00000002] (20s, 30s)
+`
+	if got := buf.String(); got != want {
+		t.Errorf("Incorrect output, want\n%sgot\n%s", want, got)
+	}
+}
+
+func TestFormatWithMissingSpans(t *testing.T) {
+	trid := id()
+	trstart := time.Date(2014, 11, 6, 13, 1, 22, 400000000, time.UTC)
+	spanIDs := make([]uniqueid.ID, 6)
+	for i := range spanIDs {
+		spanIDs[i] = id()
+	}
+	tr := vtrace.TraceRecord{
+		ID: trid,
+		Spans: []vtrace.SpanRecord{
+			{
+				ID:     spanIDs[0],
+				Parent: trid,
+				Name:   "",
+				Start:  trstart.UnixNano(),
+			},
+			{
+				ID:     spanIDs[1],
+				Parent: spanIDs[0],
+				Name:   "Child1",
+				Start:  trstart.Add(time.Second).UnixNano(),
+				End:    trstart.Add(10 * time.Second).UnixNano(),
+			},
+			{
+				ID:     spanIDs[3],
+				Parent: spanIDs[2],
+				Name:   "Decendant1",
+				Start:  trstart.Add(15 * time.Second).UnixNano(),
+				End:    trstart.Add(24 * time.Second).UnixNano(),
+			},
+			{
+				ID:     spanIDs[4],
+				Parent: spanIDs[2],
+				Name:   "Decendant2",
+				Start:  trstart.Add(12 * time.Second).UnixNano(),
+				End:    trstart.Add(18 * time.Second).UnixNano(),
+			},
+			{
+				ID:     spanIDs[5],
+				Parent: spanIDs[1],
+				Name:   "GrandChild1",
+				Start:  trstart.Add(3 * time.Second).UnixNano(),
+				End:    trstart.Add(8 * time.Second).UnixNano(),
+				Annotations: []vtrace.Annotation{
+					{
+						Message: "First Annotation",
+						When:    trstart.Add(4 * time.Second).UnixNano(),
+					},
+					{
+						Message: "Second Annotation",
+						When:    trstart.Add(6 * time.Second).UnixNano(),
+					},
+				},
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	vtrace.FormatTrace(&buf, &tr, time.UTC)
+	want := `Trace - 00000006 (2014-11-06 13:01:22.400000 UTC, ??)
+    Span - Child1 [id: 00000008 parent 00000007] (1s, 10s)
+        Span - GrandChild1 [id: 0000000c parent 00000008] (3s, 8s)
+            @4s First Annotation
+            @6s Second Annotation
+    Span - Missing Data [id: 00000000 parent 00000000] (??, ??)
+        Span - Decendant1 [id: 0000000a parent 00000009] (15s, 24s)
+        Span - Decendant2 [id: 0000000b parent 00000009] (12s, 18s)
+`
+
+	if got := buf.String(); got != want {
+		t.Errorf("Incorrect output, want\n%sgot\n%s", want, got)
+	}
+}
