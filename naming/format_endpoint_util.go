6abb3d181aaeac8ac57b24e6f976e3ce9a31b1f5
Cosmos Nicolaou <cnicolaou@google.com>
2014-11-13 14:01:16 -0800

    veyron2/naming: utility to print out endpoints from protocol, host:port args.
    Change-Id: Ic927ecd74abd1ec35d97ce29013f4b9f41c394b2

---
+++ naming/format_endpoint_util.go
@@ -0,0 +1,69 @@
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"veyron.io/veyron/veyron2/naming"
+	"veyron.io/veyron/veyron2/rt"
+
+	_ "veyron.io/veyron/veyron/profiles"
+)
+
+var protocolFlag = protocolFlagVar{"tcp"}
+var hostPortFlags = hostPortFlagVar{}
+
+type protocolFlagVar struct {
+	protocol string
+}
+
+func (pf *protocolFlagVar) Set(v string) error {
+	pf.protocol = v
+	return nil
+}
+
+func (pf *protocolFlagVar) String() string {
+	return pf.protocol
+}
+
+type addrSpec struct {
+	protocol, address string
+}
+
+type hostPortFlagVar struct {
+	addrs []addrSpec
+}
+
+func (hpf *hostPortFlagVar) Set(v string) error {
+	hpf.addrs = append(hpf.addrs, addrSpec{protocol: protocolFlag.String(), address: v})
+	return nil
+}
+
+func (hpf *hostPortFlagVar) String() string {
+	r := ""
+	for _, a := range hpf.addrs {
+		r += fmt.Sprintf("%s %s\n", a.protocol, a.address)
+	}
+	return strings.TrimRight(r, "\n")
+}
+
+func init() {
+	flag.Var(&protocolFlag, "protocol", "protocol to use for subsequent host args")
+	flag.Var(&hostPortFlags, "address", "<host:port>...")
+}
+
+func main() {
+	r := rt.Init()
+	for _, a := range hostPortFlags.addrs {
+		ep, err := r.NewEndpoint(naming.FormatEndpoint(a.protocol, a.address))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", ep, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", ep)
+	}
+}
