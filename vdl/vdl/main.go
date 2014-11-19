c491c7c8af6763a33cb3eb6ab7e4a02656e9fcfa
Robert Kroeger <rjkroege@google.com>
2014-11-19 14:03:02 -0800

    veyron/*/main.go:  update go:generate to use tool
    Use the veyron doc.go generator program to generate doc.go files for
    commands so that go:generate operates correctly when there are unit
    test source files in the same directory per:
    https://github.com/veyron/release-issues/issues/389
    
    Change-Id: I84b0a07a59b591b87717276a8528224c88d6021f

--- vdl/vdl/main.go
+++ vdl/vdl/main.go
@@ -1,11 +1,5 @@
 // The following enables go generate to generate the doc.go file.
-// Things to look out for:
-// 1) go:generate evaluates double-quoted strings into a single argument.
-// 2) go:generate performs $NAME expansion, so the bash cmd can't contain '$'.
-// 3) We generate into a *.tmp file first, otherwise go run will pick up the
-//    initially empty *.go file, and fail.
-//
-//go:generate bash -c "{ echo -e '// This file was auto-generated via go generate.\n// DO NOT UPDATE MANUALLY\n\n/*' && CMDLINE_WIDTH=80 veyron go run *.go help -style=godoc ... && echo -e '*/\npackage main'; } > ./doc.go.tmp && mv ./doc.go.tmp ./doc.go"
+//go:generate go run $VEYRON_ROOT/tools/go/src/tools/lib/cmdline/testdata/gendoc.go .
 
 package main
 
