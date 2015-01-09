83cc79b8a3368bce2bf4b176edf1395208438017
Todd Wang <toddw@google.com>
2015-01-09 11:54:30 -0800

    veyron2: Remove client from reserved functions.
    The client is no longer necessary, since it is retrieved from the
    context, and the user can simply set a new client in the context.
    
    Also added SetClient to the runtime implementation, to allow
    tests to supply a mock client in the context.
    
    Change-Id: Id7114a2f9c694f753cde9fdd506e2522c6c698d2
    MultiPart: 1/2

--- ipc/reserved/reserved.go
+++ ipc/reserved/reserved.go
@@ -7,18 +7,11 @@ import (
 	"v.io/core/veyron2/vdl/vdlroot/src/signature"
 )
 
-func cl(ctx *context.T, client ipc.Client) ipc.Client {
-	if client != nil {
-		return client
-	}
-	return veyron2.RuntimeFromContext(ctx).Client()
-}
-
 // Signature invokes the reserved signature RPC on the given name, and returns
 // the results.  The client will be used to invoke the RPC - if it is nil, the
 // default client from the runtime is used.
-func Signature(ctx *context.T, client ipc.Client, name string, opts ...ipc.CallOpt) ([]signature.Interface, error) {
-	call, err := cl(ctx, client).StartCall(ctx, name, ipc.ReservedSignature, nil, opts...)
+func Signature(ctx *context.T, name string, opts ...ipc.CallOpt) ([]signature.Interface, error) {
+	call, err := veyron2.GetClient(ctx).StartCall(ctx, name, ipc.ReservedSignature, nil, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -32,9 +25,9 @@ func Signature(ctx *context.T, client ipc.Client, name string, opts ...ipc.CallO
 // MethodSignature invokes the reserved method signature RPC on the given name,
 // and returns the results.  The client will be used to invoke the RPC - if it
 // is nil, the default client from the runtime is used.
-func MethodSignature(ctx *context.T, client ipc.Client, name, method string, opts ...ipc.CallOpt) (signature.Method, error) {
+func MethodSignature(ctx *context.T, name, method string, opts ...ipc.CallOpt) (signature.Method, error) {
 	args := []interface{}{method}
-	call, err := cl(ctx, client).StartCall(ctx, name, ipc.ReservedMethodSignature, args, opts...)
+	call, err := veyron2.GetClient(ctx).StartCall(ctx, name, ipc.ReservedMethodSignature, args, opts...)
 	if err != nil {
 		return signature.Method{}, err
 	}
