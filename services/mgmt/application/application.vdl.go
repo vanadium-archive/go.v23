62d851994d94f048180e8e30a3f1d4a375a82520
Bogdan Caprita <caprita@google.com>
2015-02-12 13:45:51 -0800

    v2/services/mgmt,v/services/mgmt: install package override (server)
    Server-side changes corresponding to go/vcl/3982.  In addition, we rename
    application PackageSpec to SignedFile, and change the application Envelope to
    use SignedFile instead of Binary + Signature fields.
    
    Change-Id: Icd19faaad48aba7665c1805dbac7d5b1ee3dcc24

--- services/mgmt/application/application.vdl.go
+++ services/mgmt/application/application.vdl.go
@@ -23,11 +23,8 @@ type Envelope struct {
 	// Args is an array of command-line arguments to be used when executing
 	// the binary.
 	Args []string
-	// Binary is an object name that identifies the application binary.
-	Binary string
-	// Signature represents a signature on the sha256 hash of the application
-	// binary by the publisher principal.
-	Signature security.Signature
+	// Binary identifies the application binary.
+	Binary SignedFile
 	// Publisher represents the set of blessings that have been bound to
 	// the principal who published this binary.
 	Publisher security.WireBlessings
@@ -53,39 +50,39 @@ func (Envelope) __VDLReflect(struct {
 //
 // Each object's media type determines how to install it.
 //
-// For example, with key=pkg1,value=PackageSpec{File:binaryrepo/configfiles} (an
+// For example, with key=pkg1,value=SignedFile{File:binaryrepo/configfiles} (an
 // archive), the "configfiles" package will be installed under the "pkg1"
-// directory. With key=pkg2,value=PackageSpec{File:binaryrepo/binfile} (a
+// directory. With key=pkg2,value=SignedFile{File:binaryrepo/binfile} (a
 // binary), the "binfile" file will be installed as the "pkg2" file.
 //
 // The keys must be valid file/directory names, without path separators.
 //
 // Any number of packages may be specified.
-type Packages map[string]PackageSpec
+type Packages map[string]SignedFile
 
 func (Packages) __VDLReflect(struct {
 	Name string "v.io/core/veyron2/services/mgmt/application.Packages"
 }) {
 }
 
-// PackageSpec represents a package that needs to be installed for an application.
-type PackageSpec struct {
-	//  File is the object name of the package.
+// SignedFile represents a file accompanied by a signature of its contents.
+type SignedFile struct {
+	//  File is the object name of the file.
 	File string
-	// Signature represents a signature on the sha256 hash of the package file
+	// Signature represents a signature on the sha256 hash of the file
 	// contents by the publisher principal.
 	Signature security.Signature
 }
 
-func (PackageSpec) __VDLReflect(struct {
-	Name string "v.io/core/veyron2/services/mgmt/application.PackageSpec"
+func (SignedFile) __VDLReflect(struct {
+	Name string "v.io/core/veyron2/services/mgmt/application.SignedFile"
 }) {
 }
 
 func init() {
 	vdl.Register((*Envelope)(nil))
 	vdl.Register((*Packages)(nil))
-	vdl.Register((*PackageSpec)(nil))
+	vdl.Register((*SignedFile)(nil))
 }
 
 // Device manager application envelopes must present this title.
