41ba714357c7a5dd0ae05ef270f22d8fe710e0ed
Alex Fandrianto <alexfandrianto@google.com>
2015-02-23 21:17:19 +0000

    TBR Revert "veyron2/vdl/javascript/codegen: Freeze the types after creation"
    This reverts commit 16c3eb179fdb5a79a5b1ef65d886d120ad2b7288.
    
    Change-Id: I2d6ff72b632f75562755b820bdad461ea6f13613

--- vdl/codegen/javascript/gen_type_def.go
+++ vdl/codegen/javascript/gen_type_def.go
@@ -9,8 +9,7 @@ import (
 // makeTypeDefinitionsString generates a string that defines the specified types.
 // It consists of the following sections:
 // - Definitions. e.g. "var _typeNamedBool = new Type();"
-// - Field assignments. e.g. "_typeNamedBool.name = \"NamedBool\";"
-// - Type Freezes, e.g. "_typedNamedBool.freeze();"
+// - Field assignments. e.g. "_typeNamedBool.name = \"NamedBool\""
 // - Constructor definitions. e.g. "types.NamedBool = Registry.lookupOrCreateConstructor(_typeNamedBool)"
 func makeTypeDefinitionsString(jsnames typeNames) string {
 	str := ""
@@ -22,10 +21,6 @@ func makeTypeDefinitionsString(jsnames typeNames) string {
 
 	for _, def := range sortedDefs {
 		str += makeTypeFieldAssignmentString(def.Name, def.Type, jsnames)
-	}
-
-	for _, def := range sortedDefs {
-		str += makeTypeFreezeString(def.Name)
 	}
 
 	for _, def := range sortedDefs {
@@ -43,14 +38,8 @@ func makeDefString(jsname string) string {
 	return fmt.Sprintf("var %s = new vdl.Type();\n", jsname)
 }
 
-// makeTypeFreezeString calls the type's freeze function to finalize it.
-// e.g. "typeNamedBool.freeze();"
-func makeTypeFreezeString(jsname string) string {
-	return fmt.Sprintf("%s.freeze();\n", jsname)
-}
-
 // makeTypeFieldAssignmentString generates assignments for type fields.
-// e.g. "_typeNamedBool.name = \"NamedBool\";"
+// e.g. "_typeNamedBool.name = \"NamedBool\""
 func makeTypeFieldAssignmentString(jsname string, t *vdl.Type, jsnames typeNames) string {
 	// kind
 	str := fmt.Sprintf("%s.kind = %s;\n", jsname, jsKind(t.Kind()))
