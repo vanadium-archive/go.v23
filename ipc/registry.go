43b2810b4410d02db374bbaf157f83df446e1544
Jungho Ahn <jhahn@google.com>
2015-02-24 13:58:35 -0800

    TBR veyron/runtimes/google/ipc/stream/fix: fix a bug in vif cache
    Currently, vifs are cached with the actual protocol not with a
    client specified protocol. E.g., vif with "wsh" are cached with
    "tcp" or "ws". So we cannot find a vif with "wsh" causing unncessary
    vif creation. There is also a similar problem with v4 and v6 protocols.
    This CL addresses this issue.
    
    MultiPart: 2/2
    Change-Id: I04052a6b8f44da422ba92f877d301ace47d2ec31

--- ipc/registry.go
+++ ipc/registry.go
@@ -1,6 +1,7 @@
 package ipc
 
 import (
+	"fmt"
 	"net"
 	"sync"
 	"time"
@@ -16,8 +17,9 @@ type DialerFunc func(protocol, address string, timeout time.Duration) (net.Conn,
 // protocol-specific string representation of the address a server will listen on.
 type ListenerFunc func(protocol, address string) (net.Listener, error)
 
-// RegisterProtocol makes available a Dialer and a Listener to
-// RegisteredNetwork.
+// RegisterProtocol makes available a Dialer and a Listener to RegisteredNetwork.
+// If the protocol represents other actual protocols, you need to specify all the
+// actual protocols. E.g, "wsh" represents "tcp4", "tcp6", "ws4", and "ws6".
 //
 // Implementations of the Manager interface are expected to use this registry
 // in order to expand the reach of the types of network protocols they can
@@ -25,11 +27,16 @@ type ListenerFunc func(protocol, address string) (net.Listener, error)
 //
 // Successive calls to RegisterProtocol replace the contents of a previous
 // call to it and returns trues if a previous value was replaced, false otherwise.
-func RegisterProtocol(protocol string, dialer DialerFunc, listener ListenerFunc) bool {
+func RegisterProtocol(protocol string, dialer DialerFunc, listener ListenerFunc, p ...string) bool {
+	// This is for handling the common case where protocol is a "singleton", to
+	// make it easier to specify.
+	if len(p) == 0 {
+		p = []string{protocol}
+	}
 	registryLock.Lock()
 	defer registryLock.Unlock()
 	_, present := registry[protocol]
-	registry[protocol] = registryEntry{dialer, listener}
+	registry[protocol] = registryEntry{dialer, listener, p}
 	return present
 }
 
@@ -38,23 +45,34 @@ func RegisterProtocol(protocol string, dialer DialerFunc, listener ListenerFunc)
 //
 // The desired protocol provided in the first argument will be passed to the
 // Dialer and Listener as the actual protocol to use when dialing or listening.
+//
+// The protocol itself must have already been registered before RegisterUnknownProtocol
+// is called, otherwise we'll panic.
 func RegisterUnknownProtocol(protocol string, dialer DialerFunc, listener ListenerFunc) bool {
+	var p []string
+	registryLock.RLock()
+	r, present := registry[protocol]
+	if !present {
+		panic(fmt.Sprintf("%s not registered", protocol))
+	}
+	p = r.p
+	registryLock.RUnlock()
 	wrappedDialer := func(_, address string, timeout time.Duration) (net.Conn, error) {
 		return dialer(protocol, address, timeout)
 	}
 	wrappedListener := func(_, address string) (net.Listener, error) {
 		return listener(protocol, address)
 	}
-	return RegisterProtocol(naming.UnknownProtocol, wrappedDialer, wrappedListener)
+	return RegisterProtocol(naming.UnknownProtocol, wrappedDialer, wrappedListener, p...)
 }
 
 // RegisteredProtocol returns the Dialer and Listener registered with a
 // previous call to RegisterProtocol.
-func RegisteredProtocol(protocol string) (DialerFunc, ListenerFunc) {
+func RegisteredProtocol(protocol string) (DialerFunc, ListenerFunc, []string) {
 	registryLock.RLock()
 	e := registry[protocol]
 	registryLock.RUnlock()
-	return e.d, e.l
+	return e.d, e.l, e.p
 }
 
 // RegisteredProtocols returns the list of protocols that have been previously
@@ -73,6 +91,7 @@ func RegisteredProtocols() []string {
 type registryEntry struct {
 	d DialerFunc
 	l ListenerFunc
+	p []string
 }
 
 var (
