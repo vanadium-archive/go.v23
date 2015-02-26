a0729b4241bfc753506e21d4dcd8584cfded85cb
Todd Wang <toddw@google.com>
2015-02-26 13:24:06 -0800

    TBR v23: Move signature printing routines to core.
    This cleans up the v23 dependencies, such that the only remaining
    non-self dependency is Cos' llog.
    
    MultiPart: 1/2
    Change-Id: I4171c74c71e286b39d47d6dd539204f9c1f41e25

--- vdlroot/init.go
+++ vdlroot/init.go
@@ -1,5 +1,8 @@
-// Package vdlroot contains import dependencies on all its sub-packages.  This
-// is meant as a convenient mechanism to pull in standard vdl packages; import
+// Package vdlroot contains the standard VDL packages; the VDLROOT environment
+// variable should point at this directory.
+//
+// This package contains import dependencies on all its sub-packages.  This is
+// meant as a convenient mechanism to pull in all standard vdl packages; import
 // vdlroot to ensure the types for all standard vdl packages are registered.
 package vdlroot
 
