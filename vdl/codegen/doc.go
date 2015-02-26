a118d1e9fafb5959e06aa0d8bd4f1a3c3eaa9c0e
Jiri Simsa <jsimsa@google.com>
2015-02-26 10:40:26 -0800

    TBR v23: Re-org vdl and vom packages.
    The main idea is that everything under v.io/v23/vdl/... has moved
    to v.io/core/veyron/..., except for the top-level v.io/v23/vdl
    package, which stays in v23.  The sub-packages consist of the vdl
    tool, and libraries to help implement the tool.
    
    The vom and vomtestgen tools also moved to v.io/core/veyron/...
    
    This makes the dependencies worse (temporarily), because of the
    vdlroot/signature package, which pulls in a bunch of
    dependencies.  That will be re-org'd separately.
    
    After that step, we'll be left with
    "github.com/cosmosnicolaou/llog" as our only external dep.
    
    MultiPart: 1/7
    Change-Id: I5bf0d7d1c0ee3ef50e7bd145a7e06966134a59dd

--- vdl/codegen/doc.go
+++
@@ -1,3 +0,0 @@
-// Package codegen implements utilities useful for all vdl code generators.
-// Code generators for specific languages live in sub-directories.
-package codegen
--- vdl/vdlutil/doc.go
+++
@@ -1,4 +0,0 @@
-// Package vdlutil provides core utilities for vdl files.  It's used by the
-// auto-generated Go code, as well as the vdl parser, compiler and code
-// generators; it should have a small set of dependencies.
-package vdlutil
