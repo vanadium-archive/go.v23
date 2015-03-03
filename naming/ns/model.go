cdbb789317a59fc29b5eda160ef492187f110724
David Why Use Two When One Will Do Presotto <p@google.com>
2015-03-02 17:24:03 -0800

    TBR: namespace:  rename Remove to Delete
    MultiPart: 1/2
    
    Change-Id: I6a0dcfddfd51cef81be2f6bb0482085df16f6a8a

--- naming/ns/model.go
+++ naming/ns/model.go
@@ -21,8 +21,9 @@ type Namespace interface {
 	// is empty, unmount all server OAs from the object name.
 	Unmount(ctx *context.T, name, server string) error
 
-	// Remove the name from the mount table and, if requested, any subtree.
-	Remove(ctx *context.T, name string, removeSubtree bool) error
+	// Delete the name from a mount table.  If the name has any children in its mount table,
+	// it (and its chidren) will only be removed if deleteSubtree is true.
+	Delete(ctx *context.T, name string, deleteSubtree bool) error
 
 	// Resolve the object name into its mounted servers.
 	Resolve(ctx *context.T, name string, opts ...naming.ResolveOpt) (entry *naming.MountEntry, err error)
