4bb377dd64fddcbb027e5cce51943223a3a12197
Asim Shankar <ashankar@google.com>
2015-03-19 18:42:54 -0700

    services/mgmt/application: Make application.Envelope JSON-codable.
    Resolves veyron/release-issues#1233
    
    MultiPart: 1/2
    Change-Id: I2619f7e6ee0861c791c1ba23c37cc6a284839994

---
+++ services/mgmt/application/application.go
@@ -0,0 +1,63 @@
+// Make the Envelope type JSON-codeable.
+
+package application
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
+	"v.io/v23/security"
+	"v.io/v23/vom"
+)
+
+type jsonType struct {
+	Title     string
+	Args      []string
+	Binary    SignedFile
+	Publisher string // base64-vom-encoded security.Blessings
+	Env       []string
+	Packages  Packages
+}
+
+func (env Envelope) MarshalJSON() ([]byte, error) {
+	var bytes []byte
+	if !env.Publisher.IsZero() {
+		var err error
+		if bytes, err = vom.Encode(env.Publisher); err != nil {
+			return nil, fmt.Errorf("failed to vom-encode Publisher: %v", err)
+		}
+	}
+	return json.Marshal(jsonType{
+		Title:     env.Title,
+		Args:      env.Args,
+		Binary:    env.Binary,
+		Publisher: base64.URLEncoding.EncodeToString(bytes),
+		Env:       env.Env,
+		Packages:  env.Packages,
+	})
+}
+
+func (env *Envelope) UnmarshalJSON(input []byte) error {
+	var jt jsonType
+	if err := json.Unmarshal(input, &jt); err != nil {
+		return err
+	}
+	var publisher security.Blessings
+	if len(jt.Publisher) > 0 {
+		bytes, err := base64.URLEncoding.DecodeString(jt.Publisher)
+		if err != nil {
+			return fmt.Errorf("failed to base64-decode Publisher: %v", err)
+		}
+		if err := vom.Decode(bytes, &publisher); err != nil {
+			return fmt.Errorf("failed to vom-decode Publisher: %v", err)
+		}
+	}
+	env.Title = jt.Title
+	env.Args = jt.Args
+	env.Binary = jt.Binary
+	env.Publisher = publisher
+	env.Env = jt.Env
+	env.Packages = jt.Packages
+	return nil
+}
