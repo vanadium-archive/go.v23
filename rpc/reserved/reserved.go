583b0ccf5ab1f929466912aff092e8d122084d0a
Ali Ghassemi <aghassemi@google.com>
2015-04-07 17:36:23 -0700

    TBR v.io/v23 Adding some doc for rpc/reserved
    Change-Id: Ibce54e8524b10550609224a802221ec973dd459e

--- rpc/reserved/reserved.go
+++ rpc/reserved/reserved.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package reserved defines interfaces for interacting with reserved RPC methods
+// such as Signature, MethodSignature and Glob.
 package reserved
 
 import (
