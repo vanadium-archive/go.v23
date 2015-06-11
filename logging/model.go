39c1d4751b8008325cfeb391f8a79bbe4a8151a8
Cosmos Nicolaou <cnicolaou@google.com>
2015-06-10 21:15:36 -0700

    v23/logging: remove the ManageLog api since it's not used anymore.
    Change-Id: Iea67730a46281b014a5aea0a909960290073c3e9

--- logging/model.go
+++ logging/model.go
@@ -92,26 +92,6 @@ type Verbosity interface {
 	}
 }
 
-type ManageLog interface {
-
-	// LogDir returns the directory where the log files are written.
-	LogDir() string
-
-	// Stats returns stats on how many lines/bytes haven been written to
-	// this set of logs.
-	Stats() (Info, Error struct{ Lines, Bytes int64 })
-
-	// ConfigureLoggerFromFlags will configure the supplied logger using
-	// command line flags.
-	ConfigureFromFlags() error
-
-	// ExplicitlySetFlags returns a map of the logging command line flags and their
-	// values formatted as strings.  Only the flags that were explicitly set are
-	// returned. This is intended for use when an application needs to know what
-	// value the flags were set to, for example when creating subprocesses.
-	ExplicitlySetFlags() map[string]string
-}
-
 type Logger interface {
 	InfoLog
 	ErrorLog
