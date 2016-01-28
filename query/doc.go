e3e90b904bb2af548608a0e96cd495c846523b44
Ali Ghassemi <aghassemi@google.com>
2016-01-28 12:38:20 -0800

    v.io/v23: Fixing the doc formatting and small wording changes.
    No list support in godoc, so I had to compromise a bit on
    formatting.
    
    before: https://screenshot.googleplex.com/pnAKbPKMJ71.png
    after: https://screenshot.googleplex.com/W1GbcmkY7Ds.png
    
    Closes https://github.com/vanadium/issues/issues/1096
    
    Change-Id: Id17ca1da9dcfe3b11b9741d61873da9e65ba1365

--- query/doc.go
+++ query/doc.go
@@ -4,27 +4,23 @@
 
 // The query packages implement Vanadium's query capabilities.
 //
-// syncql:     This package is used by client's of a Vanadium
-//             component that supports queries.
-//             It includes:
-//                 - ResultStream (which is the product of
-//                   executing a query).
-//                 - The error messages that could be returned
-//                   from performing a query.
-//                 - A function to parse an error to get the
-//                   offset into a query that caused the error.
-//             syncql does does not include the Exec function
-//             as that must be provided by the component that
-//             supports queries.
+// The syncql package is used by clients of a Vanadium
+// component that supports queries.  It includes: ResultStream
+// (which is the product of executing a query), the error messages
+// that could be returned from performing a query, and a function
+// to parse an error to get the offset into a query that
+// caused the error.  syncql does not include the Exec function
+// as that must be provided by the component that
+// supports queries.
 //
-// engine:     This is the query engine.  A component that
-//             supports queries calls the Exec function in this
-//             package.
+// The engine package implements the query engine. A component
+// that supports queries calls the Exec function in this package.
 //
-// datasource: This package contains the interfaces that a
-//             Vanadium component must implement in order to
-//             use the query engine.
+// The datasource package contains the interfaces that a
+// Vanadium component must implement in order to
+// use the query engine.
 //
-// internal:   This is internal code which implements queries.
-//             Its sole client is the engine package.
+// The internal package provides a reference implementation of the
+// queries package.
+// Its sole client is the engine package.
 package query
