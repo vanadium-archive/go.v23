513a69cb1b80b5c929148a9c50859aaf2a8ae75d
Benjamin Prosnitz <bprosnitz@google.com>
2016-04-01 17:05:15 -0700

    v23: remove references to wspr: servicerunner & security
    MultiPart: 2/2
    Change-Id: I5c6de80aea17824975bf1f9962998645f8b24cd8

--- security/blessings_override.go
+++ security/blessings_override.go
@@ -2,14 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build java android nacl wspr
+// +build java android
 //
 // We only expose the functionality below for the above build tags, to
 // discourage general usage.  The binaries that currently require this
 // functionality are our langage proxies:
 //   java/android: jni
-//   nacl:         browspr (for JavaScript)
-//   wsprd:        wsprd (for JavaScript)
 
 package security
 
